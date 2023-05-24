72dce0f4e9fe7f1ee608bfa277064ae6db5f3f8d
ekoldinho <aliekremuyar55@gmail.com>
2023-05-24 20:53:33 +0200

    Erster Commit

---
+++ src/test/java/com/pons/runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.pons.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber/report.html",
+                "junit:target/junit/junit-report.xml",
+                "rerun:target/rerun.txt",},
+        features = "src/test/resources/features",
+        glue = "com/pons/step_definitions",
+        dryRun = false,
+        stepNotifications = true,
+        tags = ""
+)
+
+public class CukesRunner {
+}
---
+++ src/test/java/com/pons/step_definitions/übersetzung_Schrittdefinitionen.java
@@ -0,0 +1,26 @@
+package com.pons.step_definitions;
+
+import com.pons.utilities.ConfigurationReader;
+import com.pons.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class übersetzung_Schrittdefinitionen {
+    @Given("Benutzer befindet sich auf der Hauptseite")
+    public void benutzerBefindedSichAufDerHauptseite() {
+    }
+
+    @When("Der Benutzer gibt {string} in das Suchfeld ein")
+    public void derBenutzerGibtInDasSuchfeldEin(String arg0) {
+    }
+
+    @Then("Der Benutzer klickt auf die Suchschaltfläche")
+    public void derBenutzerKlicktAufDieSuchschaltfläche() {
+    }
+
+    @And("Der Benutzer sieht das {string} auf der geöffneten Webseite")
+    public void derBenutzerSiehtDasAufDerGeöffnetenWebseite(String arg0) {
+    }
+}
